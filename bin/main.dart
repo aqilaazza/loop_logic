95edc955e3f0ead69e46eeffcc8c0e9d075f23c0
aqilaazza <aqilanurazza123@gmail.com>
2025-09-17 18:44:28 +0700

    feat: tambah input angka dari user

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+
 void main() {
   print("Program Dart Multi Fitur");
+  stdout.write("Masukkan sebuah angka: ");
+  int angka = int.parse(stdin.readLineSync()!);
+
+  print("Anda memasukkan angka: $angka");
 }
